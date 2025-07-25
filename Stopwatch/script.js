b956a1cdab7afaf8e9a9b60fdd6f5330849e1181
AXER77-max <axer4773@gmail.com>
2025-07-25 12:34:26 +0530

    Update script.js

--- Stopwatch/script.js
+++ Stopwatch/script.js
@@ -3,3 +3,24 @@ let timer = null;
 let startTime = 0;
 let elapsedTime = 0;
 let isRunning = false;
+
+function start(){
+    if(!isRunning){
+        startTime= Date.now() - elapsedTime;
+        timer = setInterval(update,10);
+        isRunning = true;
+    }
+}
+function stop(){
+
+}
+function reset (){
+
+}
+
+function update(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+    let hours = Math.floor(elapsedTime / (1000 * 60 *60));
+  
+  }

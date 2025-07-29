cd724ed2836d56e0e2d869c31bb8f43292be409f
AXER77-max <axer4773@gmail.com>
2025-07-29 21:09:12 +0530

    Update script.js

--- pokemon/script.js
+++ pokemon/script.js
@@ -9,20 +9,20 @@ async function fetchData() {
 
     const data = await response.json();
 
-    // Set image
+    
     const pokemonSprite = data.sprites.front_default;
     const imageElement = document.getElementById("pokemonSprite");
     imageElement.src = pokemonSprite;
     imageElement.style.display = "block";
 
-    // Set name
+    
     document.getElementById("pokemonDisplayName").textContent = data.name.toUpperCase();
 
-    // Set types
+    
     const types = data.types.map(typeInfo => typeInfo.type.name).join(", ");
     document.getElementById("pokemonTypes").textContent = types;
 
-    // Set abilities
+  
     const abilities = data.abilities.map(abilityInfo => abilityInfo.ability.name).join(", ");
     document.getElementById("pokemonAbilities").textContent = abilities;
     
